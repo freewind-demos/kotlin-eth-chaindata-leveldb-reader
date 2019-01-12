2e6a18b1b5f64d404d585c01b267c8e975260cad
freewind <nowindlee@gmail.com>
2019-01-12 16:41:23 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Kotlin LevelDB Demo
-===================
+Kotlin LevelDB Reader
+=====================
+
+Read data from a leveldb.
+
+Run `Hello.kt` in your IDE.
 
 More to see <https://github.com/fusesource/leveldbjni>
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,28 +1,18 @@
 package example
 
-import java.io.File
 import org.fusesource.leveldbjni.JniDBFactory.factory
-import org.iq80.leveldb.DB
 import org.iq80.leveldb.Options
+import java.io.File
 
+private val db = factory.open(File("./data/example.db"), Options())!!
 
 fun main(args: Array<String>) {
-    writeData(db())
-    batchWriteData(db())
-    iterate(db())
-}
 
-private fun db() = run {
-    val options = Options().apply { this.createIfMissing(true) }
-    factory.open(File("./data/example.db"), options)!!
-}
-
-private fun iterate(db: DB) {
-    println("----------- iterate -------------")
     db.use {
         val iterator = db.iterator()
         // important!
         iterator.seekToFirst()
+
         while (iterator.hasNext()) {
             val next = iterator.peekNext()
             val key = String(next.key!!)
@@ -31,25 +21,6 @@ private fun iterate(db: DB) {
             iterator.next()
         }
     }
+
 }
 
-private fun batchWriteData(db: DB) {
-    println("------------ batchWriteData -------------")
-    db.use {
-        val batch = db.createWriteBatch()
-        batch.use {
-            batch.delete("id1".toByteArray())
-            batch.put("id2".toByteArray(), "value2".toByteArray())
-            db.write(batch)
-        }
-    }
-}
-
-private fun writeData(db: DB) {
-    println("------------- writeData ---------------")
-    db.use {
-        db.put("id1".toByteArray(), "value1".toByteArray())
-        val value = String(db.get("id1".toByteArray()))
-        println(value)
-    }
-}
